8c4d6d845344384c226c7022e9edead20eec7d3b
LavanGrimwulff <31390396+LavanGrimwulff@users.noreply.github.com>
2024-11-29 18:04:03 -0600

    Lets try that again.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -8,7 +8,7 @@ const CONFIG = {
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
-   */
+   */ I don't know why this isn't working
   base: '/Portfolio/',
   projects: {
     github: {
